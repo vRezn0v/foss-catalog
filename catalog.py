1d9f79eb7d201a0c8786136d448372ad15a46b90
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2019-10-12 07:59:28 +0530

    Update gitignore

--- catalog.py
+++ catalog.py
@@ -41,7 +41,63 @@ def showLogin():
     session['state'] = state
     return render_template('login.html', client_id=CLIENT_ID, STATE=state)
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
 
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    data = json.loads(result)
+    session['provider'] = 'facebook'
+    session['username'] = data["name"]
+    session['email'] = data["email"]
+    session['facebook_id'] = data["id"]
+
+    session['access_token'] = token
+
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    session['picture'] = data["data"]["url"]
+
+    uid = getUserID(session['email'])
+    if not uid:
+        user_id = createUser(session)
+    session['uid'] = uid
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % session['username'])
+    return output
+'''
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     if request.args.get('state') != session['state']:
@@ -68,8 +124,6 @@ def gconnect():
     #token = credentials.access_token
     #url = 'https://accounts.google.com/o/oauth2/v2/auth/'
 
-
-'''
 @app.route('/oauth2callback')
 def oauth2callback():
     state  = session['state']
