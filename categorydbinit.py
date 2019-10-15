a6646438c581f0d0e98b3fbe8d820ef90856773b
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2019-10-15 22:35:14 +0530

    Added the requirements.txt file, PEP-8 Complaince

--- categorydbinit.py
+++ categorydbinit.py
@@ -12,47 +12,47 @@ session = DBSession()
 
 print '[+] Creating Categories...'
 
-aud = Category(name = "Audio", id = 1)
+aud = Category(name="Audio", id=1)
 session.add(aud)
 session.commit()
 print '[+] Created Category: Audio.'
 
-browser = Category(name = "Browsers", id = 2)
+browser = Category(name="Browsers", id=2)
 session.add(browser)
 session.commit()
 print '[+] Created Category: Browsers.'
 
-db = Category(name = "Database", id = 3)
+db = Category(name="Database", id=3)
 session.add(db)
 session.commit()
 print '[+] Created Category: Database.'
 
-dt = Category(name = "Developer Tools", id = 4)
+dt = Category(name="Developer Tools", id=4)
 session.add(dt)
 session.commit()
 print '[+] Created Category: Developer Tools.'
 
-gaming = Category(name = "Gaming", id = 5)
+gaming = Category(name="Gaming", id=5)
 session.add(gaming)
 session.commit()
 print '[+] Created Category: Gaming.'
 
-os = Category(name = "Operating Systems", id = 6)
+os = Category(name="Operating Systems", id=6)
 session.add(os)
 session.commit()
 print '[+] Created Category: Operating Systems.'
 
-rt = Category(name = "Theming/Ricing", id = 7)
+rt = Category(name="Theming/Ricing", id=7)
 session.add(rt)
 session.commit()
 print '[+] Created Category: Theming/Ricing.'
 
-sys = Category(name = "System", id = 8)
+sys = Category(name="System", id=8)
 session.add(sys)
 session.commit()
 print '[+] Created Category: System.'
 
-util = Category(name = "Utilities", id = 9)
+util = Category(name="Utilities", id=9)
 session.add(util)
 session.commit()
 print '[+] Created Category: Utilities.'
--- database_setup.py
+++ database_setup.py
@@ -8,43 +8,44 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(80), nullable = False)
-    email = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(250), nullable=False)
 
 
 class Category(Base):
     __tablename__ = 'category'
-    
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
         return {
-            'id'            :   self.id,
-            'name'          :   self.name
+            'id':   self.id,
+            'name':   self.name
             }
+
 
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    category = Column(Integer, ForeignKey('category.id'), nullable = False)
-    uid = Column(Integer, ForeignKey('user.id'), nullable = False)
+    category = Column(Integer, ForeignKey('category.id'), nullable=False)
+    uid = Column(Integer, ForeignKey('user.id'), nullable=False)
     url = Column(String(250))
 
     @property
     def serialize(self):
         return {
-            'name'          :   self.name,
-            'id'            :   self.id,
-            'category'      :   self.category,
-            'uid'           :   self.uid,
-            'description'   :   self.description,
-            'url'           :   self.url
+            'name':   self.name,
+            'id':   self.id,
+            'category':   self.category,
+            'uid':   self.uid,
+            'description':   self.description,
+            'url':   self.url
         }
 
 engine = create_engine('sqlite:///catalog.db')
